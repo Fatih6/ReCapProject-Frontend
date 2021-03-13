4108c3528363c1f95e3b8dee6bf2d5863b688a06
fatih çalıskan <fatcetev4321@gmail.com>
2021-03-13 20:40:43 +0300

    Esra hocam süpersiniz :)

---
+++ src/app/models/customer.ts
@@ -0,0 +1,7 @@
+export interface Customer{
+    customerId:number;
+    companyName:string;
+    firstName:string;
+    LastName:string;
+    email:string;
+}
---
+++ src/app/models/rental.ts
@@ -0,0 +1,12 @@
+export interface Rental{
+    rentalId:number;
+    brandName:string;
+    colorName:string;
+    modelYear:number;
+    dailyPrice:number;
+    description:string;
+    firstName:string;
+    lastName:string;
+    email:string;
+    companyName:string;
+}
